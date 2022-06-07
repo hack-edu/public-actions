12173871d8566350ebc71b4a4ff34b667f95bcf4
Noah Perks Sloan <noah@hackedu.io>
2022-06-07 13:24:54 -0500

    fix(update-images): use gojq for YAMl support

--- update-images/update.sh
+++ update-images/update.sh
@@ -1,22 +1,19 @@
 #!/usr/bin/env bash
 
-skaffoldBuild() {
-  local dir
-  dir="$1"
-  shift
-  pushd $dir >&2 || exit 1
-  skaffold build "$@" --quiet
-  popd >&2 || exit 1
-}
-
+# skaffold build if JSON is missing
 if [ x = x"${BUILD_JSON}" ]; then
-  # shellcheck disable=SC2046
-  BUILD_JSON="$(skaffoldBuild "${BUILD_DIR:-.}" "$@" | tee /dev/stderr)"
+  pushd "${BUILD_DIR:-.}" &> /dev/null || exit 1
+  BUILD_JSON="$(skaffold build "$@" --quiet | tee /dev/stderr)"
+  popd &> /dev/null || exit 1
 fi
 
-CMDS="$(echo "$BUILD_JSON" | jq --argjson images "${IMAGE_MAPPING}" -r '.builds[] |
-  . as $build | ($images | to_entries[] | select(. as $e | $build.imageName | test($e.value)) | .key) as $name |
-  "kustomize edit set image \("\($name)=\(.tag)" | @sh)"' | tee /dev/stderr)"
+CMDS="$(
+  echo "$IMAGE_MAPPING" | docker run -i --rm ghcr.io/itchyny/gojq --yaml-input --argjson builds "${BUILD_JSON}" -r '
+  . as $images | $builds | .builds[] |
+    . as $build | ($images | to_entries[] |
+        select(. as $e | $build.imageName | test($e.value))
+            | .key) as $name |
+          "kustomize edit set image \("\($name)=\(.tag)" | @sh)"' | tee /dev/stderr)"
 
 if [ x = x"${DRY_RUN:-}" ]; then
   eval "$CMDS"
