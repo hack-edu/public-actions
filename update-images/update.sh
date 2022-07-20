95bb603fc877710aac762fe4f68120c448be647d
Noah Perks Sloan <noah@hackedu.io>
2022-07-20 10:25:49 -0500

    feat(update-images): digest filter feat(update-images): pass env to skaffold build

--- update-images/update.sh
+++ update-images/update.sh
@@ -19,14 +19,24 @@ if [ x = x"${BUILD_JSON:-}" ]; then
   popd &> /dev/null || exit 1
 fi
 
+TAG_ONLY='split("@") | .[0]'
+TAG_FILTER="${TAG_FILTER:-$TAG_ONLY}"
+DIGEST='"\($imageName)@\(split("@") | .[1])"'
+if [ digest = "${TAG_FILTER}" ]; then
+  TAG_FILTER="$DIGEST"
+fi
+
 CMDS="$(
   echo "$IMAGE_MAPPING" | docker run -i --rm ghcr.io/itchyny/gojq --yaml-input --argjson builds "${BUILD_JSON}" -r '
+def tag_filter($imageName):
+  '"${TAG_FILTER}"';
+
   . as $images | $builds | .builds[] |
     . as $build | ($images | to_entries[] |
         select(. as $e | $build.imageName | test($e.value))
             | .key) as $name |
           # remove the digest from the image as it causes kustomize to include the tag in the name and confuses skaffold
-          "kustomize edit set image \("\($name)=\(.tag | split("@") | .[0])" | @sh)"' | tee "$DEBUG")"
+          "kustomize edit set image \("\($name)=\(.tag | tag_filter($build.imageName))" | @sh)"' | tee "$DEBUG")"
 
 if [ x = x"${DRY_RUN:-}" ]; then
   eval "$CMDS"
